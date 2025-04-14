7d88c31f35691a84d43b8ff5fbfdc207b5f53679
Vladimir <you@example.com>
2025-04-14 17:40:33 +0300

    Add new class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,22 @@
 package org.example;
 
+import javax.xml.namespace.QName;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+
         System.out.println("Second commit!");
 
+        Cat firstCat = new Cat("Барс", "бежевый", 2);
+        Cat secondCat = new Cat("Мурзик", "белый", 3);
+
+
+        System.out.println("данные первого кота:" + firstCat.getName() + ", " + firstCat.getColor() + ", " + firstCat.getAge());
+
+        System.out.println("данные 2-го кота:" + secondCat.getName() + ", " + secondCat.getColor() + ", " + secondCat.getAge());
     }
+
+
 }
